be40faa95917397768eb1e975e2d8a1b719d2e30
Fer-Bar <linofernando2703@gmail.com>
2025-02-25 11:34:27 -0400

    change response_file field from FilePathField to URLField in ChatBot model

--- chat_app/models.py
+++ chat_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class ChatBot(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='gemini_user')
     title = models.CharField(max_length=300, null=False)
-    response_file = models.FilePathField(null=False)
+    response_file = models.URLField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
