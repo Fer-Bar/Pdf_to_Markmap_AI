f5ddfb98c678f68cac68bb1e7181c2df4c129b19
Fer-Bar <linofernando2703@gmail.com>
2025-02-28 22:05:02 -0400

    add ChatBotResponse model with response styles and additional fields

--- chat_app/models.py
+++ chat_app/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
 
-class ChatBot(models.Model):
+class ChatBotResponse(models.Model):
+    RESPONSE_STYLES = [
+        ('CONCISE', 'Resumida'),
+        ('NORMAL', 'Normal'),
+        ('EXPLANATORY', 'Extendida')
+    ]
+
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='gemini_user')
     title = models.CharField(max_length=300, null=False)
     response_file = models.URLField(null=False)
+    response_style = models.CharField(
+        max_length=20,
+        choices=RESPONSE_STYLES,
+        default='NORMAL',
+        verbose_name='Estilo de respuesta'
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
