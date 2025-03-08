8f6810ee300eff10fc43f12c1267025acd67dd11
Fer-Bar <linofernando2703@gmail.com>
2025-03-08 19:11:46 -0400

    add STATICFILES_DIRS to settings.py for static file configuration

--- pdf_to_markmap/settings.py
+++ pdf_to_markmap/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / PROJECT_NAME / "static"]
+
 if not DEBUG:
     STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
